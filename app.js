c2921fe73a0a0b4b85550c9e93da6a38ef2fa26c
Hamid <59122894+DarkAmdHa@users.noreply.github.com>
2021-12-01 23:16:57 +0500

    Update app.js

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ window.onload = function () {
   fod.complete(function (data) {
     screenWidth = data.device["screenmmwidth"];
     screenHeight = data.device["screenmmheight"];
+    if (screenWidth != 0 && screenHeight != 0) {
+      document.querySelector(
+        ".screenWidth"
+      ).innerHTML = `<p>Found Width and Height</p> <p>Screen Width: ${screenWidth}</p> <p>Screen Height: ${screenHeight}</p>`;
+      const ratioW = (screenWidth * 3.77) / window.innerWidth;
+      const size = 377.95275591;
+      document.querySelector(".line").style.width = size * ratioW + "px";
+    }
   });
 };
-if (screenWidth != 0 && screenHeight != 0) {
-  document.querySelector(
-    ".screenWidth"
-  ).innerHTML = `<p>Found Width and Height</p> <p>Screen Width: ${screenWidth}</p> <p>Screen Height: ${screenHeight}</p>`;
-  const ratioW = (screenWidth * 3.77) / window.innerWidth;
-  const size = 377.95275591;
-  document.querySelector(".line").style.width = size * ratioW + "px";
-}
